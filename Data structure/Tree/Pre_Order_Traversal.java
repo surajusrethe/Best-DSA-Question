8b20808b1a74e47e61bdd6d024b125259f283a9e
Suraj Usrethe <104275415+surajusrethe@users.noreply.github.com>
2023-04-01 02:28:57 +0530

    preeorder
    pre

---
+++ Data structure/Tree/Pre_Order_Traversal.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root)
+    {
+        pre(root);
+        return list;
+    }
+
+    void pre(TreeNode root)
+
+    {
+        if(root == null) return;
+
+        list.add(root.val);
+        pre(root.left);
+        pre(root.right);
+    }
+}
