c80a768eb40c721c250f59a81a27f865be69397e
Suraj Usrethe <104275415+surajusrethe@users.noreply.github.com>
2023-04-18 05:14:00 +0530

    Star Staircase pattern.java
    Star Staircase pattern.java

--- Data structure/Patterns.java/Star Staircase pattern.java
+++ Data structure/Patterns.java/Star Staircase pattern.java
@@ -15,10 +15,33 @@ Output:
 
 /************************************************************ Approach **************************************************************************/
 
+/*
+- Loop through each row: The outer loop is used to loop through each row, from 1 to n in the first half, and from n-1 to 1 in the second half.
 
+- Print stars: The inner loop is used to print the stars in each row, starting from 1 in the first row, and increasing by 1 for each subsequent row in the first half,   and starting from n-1 in the second-last row, and decreasing by 1 for each subsequent row in the second half.
+
+- Move to next line: After printing the stars for each row, we move to the next line using the System.out.println() statement. This is to ensure that each row is         printed on a new line.
+*/
 
 /************************************************************ Code **************************************************************************/
 
+public class Pattern {
+    public static void printTriangle(int n) {
+        for (int i = 1; i <= n; i++) {   // For each row
+            for (int j = 1; j <= i; j++) {   // Print stars
+                System.out.print("* ");
+            }
+            System.out.println();   // Move to next line
+        }
+
+        for (int i = n-1; i >= 1; i--) {   // For each row
+            for (int j = 1; j <= i; j++) {   // Print stars
+                System.out.print("* ");
+            }
+            System.out.println();   // Move to next line
+        }
+    }
+}
 
 
 /************************************************************ Dry run **************************************************************************/
