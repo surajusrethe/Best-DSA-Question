71a35288d0aab3bd77d26048c846aa0ca1e99fbb
Suraj Usrethe <104275415+surajusrethe@users.noreply.github.com>
2023-07-17 23:04:13 +0530

    Update Greater on right side.java

--- Data structure/Array/Greater on right side.java
+++ Data structure/Array/Greater on right side.java
@@ -1,4 +1,4 @@
-/******************************************************** Question *************************************************************/
+/******************************************************** Question ************************************************************/
 /*
 You are given an array Arr of size N. Replace every element with the next greatest element (greatest element on its right side) in the array. Also, since there is no element next to the last element, replace it with -1.
 
