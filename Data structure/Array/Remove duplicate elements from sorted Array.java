611ff832cfc0d772c610be12c15e3df4ef9bf34b
Suraj Usrethe <104275415+surajusrethe@users.noreply.github.com>
2023-07-20 09:04:21 +0530

    Update Remove duplicate elements from sorted Array.java

--- Data structure/Array/Remove duplicate elements from sorted Array.java
+++ Data structure/Array/Remove duplicate elements from sorted Array.java
@@ -1,4 +1,4 @@
-/******************************************************** Question **************************************************************/
+/******************************************************* Question **************************************************************/
 /*
 
 Given a sorted array A[] of size N, delete all the duplicated elements from A[]. Modify the array such that if there are X distinct elements in it then the first X positions of the array should be filled with them in increasing order and return the number of distinct elements in the array.
